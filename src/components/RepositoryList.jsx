792d94d8dbba486d8e9e6c79c5a44c7ebacb08f8
Juan Veronez <juan.monteirov@gmail.com>
2021-09-15 08:48:41 -0300

    Creating my "first" component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,24 @@
+export function RepositoryList() {
+  return (
+    <section>
+      <h1>Lista de repositórios</h1>
+      <ul>
+        <li>
+          <strong></strong>
+          <p>Component in ReactJS</p>
+          <a href="">Acessar repositório</a>
+        </li>
+        <li>
+          <strong></strong>
+          <p>Component in ReactJS</p>
+          <a href="">Acessar repositório</a>
+        </li>
+        <li>
+          <strong></strong>
+          <p>Component in ReactJS</p>
+          <a href="">Acessar repositório</a>
+        </li>
+      </ul>
+    </section>
+  )
+}
